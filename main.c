81d1477376702340971ea54afbe6b2a1d5804547
sayed azp <sayedazp@gmail.com>
2023-03-28 23:27:21 +0200

    task0

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-	int len = _printf("%sfive", "five\n", '\n');
-	int len2 = printf("%sfive", "five\n", '\n');
-	printf("len1 = %d\n len2 = %d", len, len2);
-	return (0);
-}
